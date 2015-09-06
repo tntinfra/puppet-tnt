ed90073d56f40b7e978806aecc442d153dd99d58
Toni Schmidbauer <toni@stderr.at>
2015-09-06 21:55:15 +0200

    use single quotes for hostnames

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,11 @@
-node noc.tntinfra.net {
+node 'noc.tntinfra.net' {
   include role::noc
 }
 
-node sippma.tntinfra.net {
+node 'sippma.tntinfra.net' {
   include role::sippma::base
 }
 
-node sippma-db.tntinfra.net {
+node 'sippma-db.tntinfra.net' {
   include role::sippma::db
 }
