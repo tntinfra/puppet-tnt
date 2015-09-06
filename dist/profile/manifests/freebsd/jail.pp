b71b0af04e36932d9dc22bf0fb75a84aeafc2926
Toni Schmidbauer <toni@stderr.at>
2015-09-06 20:15:56 +0200

    added cloned interface lo1 to rc.conf

--- dist/profile/manifests/freebsd/jail.pp
+++ dist/profile/manifests/freebsd/jail.pp
@@ -6,4 +6,10 @@ class profile::freebsd::jail {
   package { 'iocage':
     ensure => 'installed',
   }
+
+  file_line { 'clone_lo1_interface':
+    ensure => 'present',
+    path   => '/etc/rc.conf'
+    line   => 'cloned_interfaces="${cloned_interfaces} lo1"'
+  }
 }
