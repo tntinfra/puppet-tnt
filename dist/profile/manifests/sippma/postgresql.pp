94843627b543f5178c9ad670df9bb473da071add
Toni Schmidbauer <toni@stderr.at>
2015-09-07 17:15:07 +0200

    install postgresql server/client

--- dist/profile/manifests/sippma/postgresql.pp
+++ dist/profile/manifests/sippma/postgresql.pp
@@ -1,4 +1,10 @@
 # sippma postgresql installation
 class profile::sippma::postgresql {
+  package { 'postgresql94-server':
+    ensure => 'installed'
+  }
 
+  package { 'postgresql94-client':
+    ensure => 'installed',
+  }
 }
