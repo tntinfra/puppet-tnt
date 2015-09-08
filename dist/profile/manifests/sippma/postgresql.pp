d481495958b67e846afdcac7151654c584535b8b
Toni Schmidbauer <toni@stderr.at>
2015-09-08 20:32:55 +0200

    fixed file source for pgsql configs

--- dist/profile/manifests/sippma/postgresql.pp
+++ dist/profile/manifests/sippma/postgresql.pp
@@ -8,11 +8,11 @@ class profile::sippma::postgresql {
   } ->
   file { '/usr/local/pgsql/data/postgresql.conf':
     ensure => 'file',
-    source => 'puppet:///modules/profile/files/postgresql.conf',
+    source => 'puppet:///modules/profile/postgresql.conf',
   } ->
   file { '/usr/local/pgsql/data/pg_hba.conf':
     ensure => 'file',
-    source => 'puppet:///modules/profile/files/pg_hba.conf',
+    source => 'puppet:///modules/profile/pg_hba.conf',
   } ->
   file_line { 'postgresql_startup':
     ensure => 'present',
