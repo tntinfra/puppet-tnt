d28c0b1a40038db9a28742ef6999246e87edb3c8
Toni Schmidbauer <toni@stderr.at>
2015-09-08 20:24:13 +0200

    install config files, start service

--- dist/profile/manifests/sippma/postgresql.pp
+++ dist/profile/manifests/sippma/postgresql.pp
@@ -2,9 +2,25 @@
 class profile::sippma::postgresql {
   package { 'postgresql94-server':
     ensure => 'installed'
-  }
-
+  } ->
   package { 'postgresql94-client':
     ensure => 'installed',
+  } ->
+  file { '/usr/local/pgsql/data/postgresql.conf':
+    ensure => 'file',
+    source => 'puppet:///modules/profile/files/postgresql.conf',
+  } ->
+  file { '/usr/local/pgsql/data/pg_hba.conf':
+    ensure => 'file',
+    source => 'puppet:///modules/profile/files/pg_hba.conf',
+  } ->
+  file_line { 'postgresql_startup':
+    ensure => 'present',
+    path => '/etc/rc.conf',
+    line => 'postgresql_enable="YES"'
+  } ->
+  service { 'postgresql':
+    ensure => 'running,'
+    enable => true,
   }
 }
