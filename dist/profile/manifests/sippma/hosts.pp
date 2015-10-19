4dd7d7392b509adad3a44380e42233c1a6dfd7b3
Toni Schmidbauer <toni@stderr.at>
2015-10-19 16:58:32 +0200

    added web and db hosts

--- dist/profile/manifests/sippma/hosts.pp
+++ dist/profile/manifests/sippma/hosts.pp
@@ -10,4 +10,16 @@ class profile::sippma::hosts {
     ip     => '192.168.122.102',
     host_aliases => 'sippma.tntinfra.net'
   }
+
+  host { 'web':
+    ensure => present,
+    ip     => '192.168.122.103',
+    host_aliases => 'web.sippma.tntinfra.net'
+  }
+
+  host { 'db':
+    ensure => present,
+    ip     => '192.168.122.104',
+    host_aliases => 'db.sippma.tntinfra.net'
+  }
 }
