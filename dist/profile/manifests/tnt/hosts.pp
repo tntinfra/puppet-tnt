f0e2e6332bae63521e820952107599f2365ff3c7
Toni Schmidbauer <toni@stderr.at>
2016-03-05 19:06:31 +0100

    fixed host resource

--- dist/profile/manifests/tnt/hosts.pp
+++ dist/profile/manifests/tnt/hosts.pp
@@ -1,7 +1,7 @@
 # add hosts to /etc/hosts
 #
 class profile::tnt::hosts {
-  hosts { 'stdout':
+  host { 'stdout':
     ensure       => present,
     ip           => '192.168.133.3',
     host_aliases => [ 'git', 'stdout.stderr.at', 'stderr.at']
