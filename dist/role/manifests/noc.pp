6c1efcc2f50f101aca5f53615d3f8ae9620c94c6
Toni Schmidbauer <toni@stderr.at>
2016-03-05 19:58:04 +0100

    added elasticsearch profile

--- dist/role/manifests/noc.pp
+++ dist/role/manifests/noc.pp
@@ -2,7 +2,7 @@
 class role::noc {
   contain profile::tnt::common
   contain profile::tnt::hosts
+  contain profile::tnt::elasticsearch
   contain profile::sippma::hosts
   contain profile::puppetmaster
-
 }
