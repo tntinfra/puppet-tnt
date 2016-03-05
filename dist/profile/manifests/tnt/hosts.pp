d73b8674803129e3feeae244ca6a7e92028a93ce
Toni Schmidbauer <toni@stderr.at>
2016-03-05 19:48:39 +0100

    added git.stderr.at

--- dist/profile/manifests/tnt/hosts.pp
+++ dist/profile/manifests/tnt/hosts.pp
@@ -4,7 +4,7 @@ class profile::tnt::hosts {
   host { 'stdout':
     ensure       => present,
     ip           => '192.168.133.3',
-    host_aliases => [ 'git', 'stdout.stderr.at', 'stderr.at']
+    host_aliases => [ 'git.stderr.at', 'stdout.stderr.at', 'stderr.at']
   }
 
 }
